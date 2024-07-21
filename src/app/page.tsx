1e1fc1166fcb43fc27465fe9ffb8acd9d1b285c5
darkdra771 <bramlim2407@gmail.com>
2024-07-21 23:09:21 +0800

    DEV-13 initial integration with NextAuth.JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { SignIn } from "@/components/auth/signIn";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hello
+        <SignIn />
       </div>
     </main>
   );
